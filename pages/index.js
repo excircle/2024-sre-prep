a33904388c6c4c6733ffd9912672ad8db80008f8
excircle <unixdisciple.us@gmail.com>
2024-04-19 12:56:18 -0700

    SPA front end is functional. Routing is working.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import React from "react"
+import { useRouter } from 'next/router';
+import HomePage from "./homepage/HopePage";
+import Basics from "./basics/Basics";
 
 export default function Home() {
+  const router = useRouter();
+
+  const renderComponent = () => {
+    switch (router.pathname) {
+      case '/':
+        return <HomePage />;
+      case '/basics':
+        return <Basics />;
+      default:
+        return <div>Page not found</div>;
+    }
+  };
+
   return (
     <React.Fragment>
       <p className="text-2xl">SRE Prep</p>
       <hr></hr>
+      <div>
+        <h1>Next.js Example</h1>
+        {renderComponent()}
+      </div>
     </React.Fragment>
   )
 }
