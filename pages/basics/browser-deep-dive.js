662b91f93f30fc9ed2f1719734669b6ddf69a857
Alexander Kalaj <adkalaj@gmail.com>
2024-04-19 12:38:05 -0700

    Initial commit

---
+++ pages/basics/browser-deep-dive.js
@@ -0,0 +1,5 @@
+export default function Browser() {
+    return (
+        <p className="text-2xl">This is the Browser Deep Dive Page!</p>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <p className="text-2xl">SRE Prep</p>
+      <hr></hr>
+    </React.Fragment>
+  )
+}
